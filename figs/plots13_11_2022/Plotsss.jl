2ffef80425957890ca7a4ee99d3c2ffbd3478f59
Giulio Benedetti <giuliobene2000@gmail.com>
2022-11-13 18:26:57 +0100

    Improve code for randparam benchmark plots

--- figs/plots13_11_2022/Plotsss.jl
+++ figs/plots13_11_2022/Plotsss.jl
@@ -1,64 +1,60 @@
 using Plots, CSV, DataFrames
 using StatsPlots
 theme(:ggplot2)
-# Data from Matlab
-M_Ex1 = CSV.read("RndEx1.csv", DataFrame, header = 1)
-M_Ex2 = CSV.read("RndEx2.csv", DataFrame, header = 1)
-M_Ex3 = CSV.read("RndEx3.csv", DataFrame, header = 1)
-M_Ex4 = CSV.read("RndEx4.csv", DataFrame, header = 1)
 
-M_NonStiff = CSV.read("BenchNonStiff.csv", DataFrame, header = 0)
-M_Stiff = CSV.read("BenchStiff.csv", DataFrame, header = 0)
-M_Harmonic = CSV.read("BenchHarmonic.csv", DataFrame, header = 0)
-M_SIR = CSV.read("BenchSIR.csv", DataFrame, header = 0)
-M_LV = CSV.read("BenchLV.csv", DataFrame, header = 0)
+# Data from Matlab
+matlab_dir = "data_matlab"
+
+M_NonStiff = CSV.read(joinpath(matlab_dir, "BenchNonStiff.csv"), DataFrame, header = 0)
+M_Stiff = CSV.read(joinpath(matlab_dir, "BenchStiff.csv"), DataFrame, header = 0)
+M_Harmonic = CSV.read(joinpath(matlab_dir, "BenchHarmonic.csv"), DataFrame, header = 0)
+M_SIR = CSV.read(joinpath(matlab_dir, "BenchSIR.csv"), DataFrame, header = 0)
+M_LV = CSV.read(joinpath(matlab_dir, "BenchLV.csv"), DataFrame, header = 0)
 
 # Data from Julia
-J_Nonstiff_E1 = CSV.read("NonStiff_E1.csv", DataFrame, header = 1)
-J_Nonstiff_E2 = CSV.read("NonStiff_E2.csv", DataFrame, header = 1)
-J_Nonstiff_T1 = CSV.read("NonStiff_T1.csv", DataFrame, header = 1)
-J_Nonstiff_T2 = CSV.read("NonStiff_T2.csv", DataFrame, header = 1)
+julia_dir = "data_Julia"
 
-J_stiff_E1 = CSV.read("Stiff_E1.csv", DataFrame, header = 1)
-J_stiff_E2 = CSV.read("Stiff_E2.csv", DataFrame, header = 1)
-J_stiff_T1 = CSV.read("Stiff_T1.csv", DataFrame, header = 1)
-J_stiff_T2 = CSV.read("Stiff_T2.csv", DataFrame, header = 1)
+J_Nonstiff_E1 = CSV.read(joinpath(julia_dir, "NonStiff_E1.csv"), DataFrame, header = 1)
+J_Nonstiff_E2 = CSV.read(joinpath(julia_dir, "NonStiff_E2.csv"), DataFrame, header = 1)
+J_Nonstiff_T1 = CSV.read(joinpath(julia_dir, "NonStiff_T1.csv"), DataFrame, header = 1)
+J_Nonstiff_T2 = CSV.read(joinpath(julia_dir, "NonStiff_T2.csv"), DataFrame, header = 1)
 
-J_Harmonic_E1 = CSV.read("Harmonic_E1.csv", DataFrame, header = 1)
-J_Harmonic_E2 = CSV.read("Harmonic_E2.csv", DataFrame, header = 1)
-J_Harmonic_T1 = CSV.read("Harmonic_T1.csv", DataFrame, header = 1)
-J_Harmonic_T2 = CSV.read("Harmonic_T2.csv", DataFrame, header = 1)
+J_Nonstiff = innerjoin(J_Nonstiff_E1, J_Nonstiff_E2, )
 
-J_LV_E1 = CSV.read("LV_E1.csv", DataFrame, header = 1)
-J_LV_E2 = CSV.read("LV_E2.csv", DataFrame, header = 1)
-J_LV_T1 = CSV.read("LV_T1.csv", DataFrame, header = 1)
-J_LV_T2 = CSV.read("LV_T2.csv", DataFrame, header = 1)
-DynLV = CSV.read("DynLV.csv", DataFrame, header = 1)
+J_stiff_E1 = CSV.read(joinpath(julia_dir, "Stiff_E1.csv"), DataFrame, header = 1)
+J_stiff_E2 = CSV.read(joinpath(julia_dir, "Stiff_E2.csv"), DataFrame, header = 1)
+J_stiff_T1 = CSV.read(joinpath(julia_dir, "Stiff_T1.csv"), DataFrame, header = 1)
+J_stiff_T2 = CSV.read(joinpath(julia_dir, "Stiff_T2.csv"), DataFrame, header = 1)
 
-J_SIR_E1 = CSV.read("SIR_E1.csv", DataFrame, header = 1)
-J_SIR_E2 = CSV.read("SIR_E2.csv", DataFrame, header = 1)
-J_SIR_T1 = CSV.read("SIR_T1.csv", DataFrame, header = 1)
-J_SIR_T2 = CSV.read("SIR_T2.csv", DataFrame, header = 1)
-# J_SIR_E3 = CSV.read("SIR_E3.csv", DataFrame, header = 1)
-J_SIR_E4 = CSV.read("SIR_E4.csv", DataFrame, header = 1)
-# J_SIR_T3 = CSV.read("SIR_T3.csv", DataFrame, header = 1)
-J_SIR_T4 = CSV.read("SIR_T4.csv", DataFrame, header = 1)
-J_SIR_E5 = CSV.read("SIR_E5.csv", DataFrame, header = 1)
-J_SIR_E6 = CSV.read("SIR_E6.csv", DataFrame, header = 1)
-J_SIR_T5 = CSV.read("SIR_T5.csv", DataFrame, header = 1)
-J_SIR_T6 = CSV.read("SIR_T6.csv", DataFrame, header = 1)
-J_SIR_E7 = CSV.read("SIR_E7.csv", DataFrame, header = 1)
-J_SIR_E8 = CSV.read("SIR_E8.csv", DataFrame, header = 1)
-J_SIR_E9 = CSV.read("SIR_E9.csv", DataFrame, header = 1)
-J_SIR_T7 = CSV.read("SIR_T7.csv", DataFrame, header = 1)
-J_SIR_T8 = CSV.read("SIR_T8.csv", DataFrame, header = 1)
-J_SIR_T9 = CSV.read("SIR_T9.csv", DataFrame, header = 1)
+J_Harmonic_E1 = CSV.read(joinpath(julia_dir, "Harmonic_E1.csv"), DataFrame, header = 1)
+J_Harmonic_E2 = CSV.read(joinpath(julia_dir, "Harmonic_E2.csv"), DataFrame, header = 1)
+J_Harmonic_T1 = CSV.read(joinpath(julia_dir, "Harmonic_T1.csv"), DataFrame, header = 1)
+J_Harmonic_T2 = CSV.read(joinpath(julia_dir, "Harmonic_T2.csv"), DataFrame, header = 1)
 
-J_tRnd1=CSV.read("tRnd1.csv", DataFrame, header = 1)
-J_tRnd2=CSV.read("tRnd2.csv", DataFrame, header = 1)
-J_ErrRnd1=CSV.read("ErrRnd1.csv", DataFrame, header = 1)
-J_ErrRnd2=CSV.read("ErrRnd2.csv", DataFrame, header = 1)
+J_LV_E1 = CSV.read(joinpath(julia_dir, "LV_E1.csv"), DataFrame, header = 1)
+J_LV_E2 = CSV.read(joinpath(julia_dir, "LV_E2.csv"), DataFrame, header = 1)
+J_LV_T1 = CSV.read(joinpath(julia_dir, "LV_T1.csv"), DataFrame, header = 1)
+J_LV_T2 = CSV.read(joinpath(julia_dir, "LV_T2.csv"), DataFrame, header = 1)
+DynLV = CSV.read(joinpath(julia_dir, "DynLV.csv"), DataFrame, header = 1)
 
+J_SIR_E1 = CSV.read(joinpath(julia_dir, "SIR_E1.csv"), DataFrame, header = 1)
+J_SIR_E2 = CSV.read(joinpath(julia_dir, "SIR_E2.csv"), DataFrame, header = 1)
+J_SIR_T1 = CSV.read(joinpath(julia_dir, "SIR_T1.csv"), DataFrame, header = 1)
+J_SIR_T2 = CSV.read(joinpath(julia_dir, "SIR_T2.csv"), DataFrame, header = 1)
+# J_SIR_E3 = CSV.read(joinpath(julia_dir,"SIR_E3.csv"), DataFrame, header = 1)
+J_SIR_E4 = CSV.read(joinpath(julia_dir,"SIR_E4.csv"), DataFrame, header = 1)
+# J_SIR_T3 = CSV.read(joinpath(julia_dir,"SIR_T3.csv"), DataFrame, header = 1)
+J_SIR_T4 = CSV.read(joinpath(julia_dir, "SIR_T4.csv"), DataFrame, header = 1)
+J_SIR_E5 = CSV.read(joinpath(julia_dir, "SIR_E5.csv"), DataFrame, header = 1)
+J_SIR_E6 = CSV.read(joinpath(julia_dir, "SIR_E6.csv"), DataFrame, header = 1)
+J_SIR_T5 = CSV.read(joinpath(julia_dir, "SIR_T5.csv"), DataFrame, header = 1)
+J_SIR_T6 = CSV.read(joinpath(julia_dir, "SIR_T6.csv"), DataFrame, header = 1)
+J_SIR_E7 = CSV.read(joinpath(julia_dir, "SIR_E7.csv"), DataFrame, header = 1)
+J_SIR_E8 = CSV.read(joinpath(julia_dir, "SIR_E8.csv"), DataFrame, header = 1)
+J_SIR_E9 = CSV.read(joinpath(julia_dir, "SIR_E9.csv"), DataFrame, header = 1)
+J_SIR_T7 = CSV.read(joinpath(julia_dir, "SIR_T7.csv"), DataFrame, header = 1)
+J_SIR_T8 = CSV.read(joinpath(julia_dir, "SIR_T8.csv"), DataFrame, header = 1)
+J_SIR_T9 = CSV.read(joinpath(julia_dir, "SIR_T9.csv"), DataFrame, header = 1)
 
 ## plot Examples
 
@@ -144,51 +140,58 @@ PltMD=plot(p4, p5, p6, layout = l,size = (600, 500))
 
 #### plot randoms
 
-scatter(Vector(J_tRnd1[:,1]), Vector(J_ErrRnd1[:,1]), xscale = :log, yscale = :log, linewidth = 3, markersize = 5,
-     label = "J-PC", shape = :circle, xlabel="Execution time (sc, Log)", ylabel="Error: 2-norm (Log)",
-     thickness_scaling = 1,legend_position= :bottomleft, fc=:transparent,framestyle=:box)
-     scatter!(Vector(J_tRnd2[:,1]), Vector(J_ErrRnd2[:,1]),linewidth = 3, markersize = 5,label = "J-NR", shape = :rect)
+random_params_benchmark = CSV.read("data/random_params_benchmark.csv", DataFrame, header = 1)
 
-    Mt1=vcat(M_Ex1.Bench1_1,M_Ex2.Bench2_1,M_Ex3.Bench3_1,M_Ex4.Bench4_1)
-    Merr1=vcat(M_Ex1.Bench1_5,M_Ex2.Bench2_5,M_Ex3.Bench3_5,M_Ex4.Bench4_5)
+for (plot_idx, method) in enumerate(unique(random_params_benchmark.Method))
+    method_subset = filter(:Method => x -> x == method, random_params_benchmark)
+    if plot_idx == 1
+        p7 = scatter(method_subset.ExecutionTime,
+                    method_subset.Error,
+                    scale = :log,
+                    label = method,
+                    markersize = 3,
+                    title = "(a)",
+                    xlabel = "Execution time (sc, Log)",
+                    ylabel = "Error: 2-norm (Log)",
+                    legendposition = :bottomleft)
+    else
+        scatter!(method_subset.ExecutionTime,
+                 method_subset.Error,
+                 label = method,
+                 markersize = 3)
+    end
+    display(p)
+end
 
-    Mt2=vcat(M_Ex1.Bench1_2,M_Ex2.Bench2_2,M_Ex3.Bench3_2,M_Ex4.Bench4_2)
-    Merr2=vcat(M_Ex1.Bench1_6,M_Ex2.Bench2_6,M_Ex3.Bench3_6,M_Ex4.Bench4_6)
+p8 = @df random_params_benchmark boxplot(:Method, :Error,
+                                         yscale = :log,
+                                         ylabel = "Error: 2-norm (Log)",
+                                         c = :black,
+                                         fillcolor = :white,
+                                         legend = false,
+                                         title = "(b)",
+                                         titleloc = :left,
+                                         titlefont = font(10))
 
-    Mt3=vcat(M_Ex1.Bench1_3,M_Ex2.Bench2_3,M_Ex3.Bench3_3,M_Ex4.Bench4_3)
-    Merr3=vcat(M_Ex1.Bench1_7,M_Ex2.Bench2_7,M_Ex3.Bench3_7,M_Ex4.Bench4_7)
+p9 = @df random_params_benchmark boxplot(:Method, :ExecutionTime,
+                                         yscale = :log,
+                                         ylabel = "Execution time (sc, Log)",
+                                         c = :black,
+                                         fillcolor = :white,
+                                         legend = false,
+                                         title = "(c)",
+                                         titleloc = :left,
+                                         titlefont = font(10))
 
-    Mt4=vcat(M_Ex1.Bench1_4,M_Ex2.Bench2_4,M_Ex3.Bench3_4,M_Ex4.Bench4_4)
-    Merr4=vcat(M_Ex1.Bench1_8,M_Ex2.Bench2_8,M_Ex3.Bench3_8,M_Ex4.Bench4_8)
+l3 = @layout [b{.5w} grid(2,1)]
+PltRnd = plot(p7, p8, p9,
+              layout = l3,
+              size = (800, 500))
 
-    scatter!(Mt1, Merr1, linewidth = 3, markersize = 5,label = "M-PI-EX",shape = :rtriangle)
-    scatter!(Mt3, Merr3, linewidth = 3, markersize = 5,label = "M-PI-IM1", shape = :diamond)
-    scatter!(Mt2, Merr2, linewidth = 3, markersize = 5,label = "M-PI-PC", shape = :circle)
-    p7=scatter!(Mt4, Merr4, linewidth = 3, markersize = 5,label = "M-PI-IM2", shape = :rect,
-    title = "(a)", titleloc = :left, titlefont = font(10))
-
-X = ["J-PC", "J-NR","M-PI-EX","M-PI-IM1","M-PI-PC","M-PI-IM2"]
-    Y=[Vector(J_ErrRnd1[:,1]),Vector(J_ErrRnd2[:,1]),Merr1,Merr2,Merr3,Merr4]
-    X2 = [fill(x,length(y)) for (x,y) in zip(X,Y)]
-    df = DataFrame(X = X2, Y = Y)
-    p8 = @df df boxplot(:X, :Y, c=:black, fillcolor=:white, legend=false,
-    title = "(b)", titleloc = :left, titlefont = font(10),
-    yscale=:log, ylabel="Error: 2-norm (Log)")
-
-X = ["J-PC", "J-NR","M-PI-EX","M-PI-IM1","M-PI-PC","M-PI-IM2"]
-        Y=[Vector(J_tRnd1[:,1]),Vector(J_tRnd2[:,1]),Mt1,Mt2,Mt3,Mt4]
-        X2 = [fill(x,length(y)) for (x,y) in zip(X,Y)]
-        df = DataFrame(X = X2, Y = Y)
-        p9 = @df df boxplot(:X, :Y, c=:black, fillcolor=:white, legend=false,
-        title = "(c)", titleloc = :left, titlefont = font(10),
-        yscale=:log, ylabel="Execution time (sc, Log)")
-
-l = @layout [b{.5w} grid(2,1)]
-PltRnd=plot(p7, p8, p9, layout = l,size = (800, 500))
+savefig(PltRnd, "PltRnd.png")
+savefig(PltRnd, "PltRnd.svg")
 
 savefig(Plt1D,"Plt1D.svg")
 savefig(PltMD,"PltMD.svg")
-savefig(PltRnd,"PltRnd.svg")
 savefig(Plt1D,"Plt1D.png")
 savefig(PltMD,"PltMD.png")
-savefig(PltRnd,"PltRnd.png")
