98dd6b3bcabfcad2f03f93289d91c1989360f958
Giulio Benedetti <giuliobene2000@gmail.com>
2022-12-14 22:02:20 +0100

    Merge repos

---
+++ run_covid_fitting.jl
@@ -0,0 +1,27 @@
+# reference for model
+# https://doi.org/10.1016/j.chaos.2020.109846
+# https://doi.org/10.1016/j.chaos.2021.110652
+# https://doi.org/10.3390/axioms10030135
+
+using Optim, StatsBase
+using FdeSolver
+using Plots, StatsPlots, StatsPlots.PlotMeasures
+using SpecialFunctions
+using CSV, HTTP, DataFrames, Dates
+
+source_dir = joinpath(@__DIR__, "src/covid_fitting")
+output_dir = joinpath(@__DIR__, "results/covid_fitting")
+
+# dataset of Covid from CSSE
+repo = HTTP.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+dataset_CC = CSV.read(repo.body, DataFrame) # all data of confirmed
+
+# run models
+include(joinpath(source_dir, "SEIPAHRFPortugal.jl")) # Portugal
+include(joinpath(source_dir, "SEIPAHRFSpain.jl"))    # Spain
+
+# make plot
+P = plot(plSpain, plPortugal,
+    layout=grid(2, 1), size=(700, 650))
+
+savefig(P, joinpath(output_dir, "CovidSpnPrt.svg"))
